bfb8a47907e57b4a29c0cea6faf00d2dfcde9431
ShkAlexO <shk.alex.o@gmail.com>
2025-03-23 23:45:06 +0200

    fourth hw

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,14 @@ import {HW1} from './HW/1/HW1';
 import {HW2} from './HW/2/HW2';
 import {HW3} from './HW/3/HW3';
 import {HW4} from './HW/4/HW4';
-import {Counter} from "./HW/examination/Counter/Counter";
-import {CounterSecond} from "./HW/examination_2/CounterSecond/CounterSecond";
-
 
 function App() {
     return (
         <div className="App">
-            {/*<HW1/>*/}
-            {/*<HW2/>*/}
-            {/*<HW3/>*/}
+            <HW1/>
+            <HW2/>
+            <HW3/>
             <HW4 />
-            {/*<Counter/>*/}
-            {/*<CounterSecond/>*/}
         </div>
     );
 }
